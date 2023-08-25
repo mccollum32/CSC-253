202f82ee47eb57486d7bb0cb6c795fc05c1d395f
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2023-08-25 05:45:49 -0400

    Add files via upload

---
+++ m1h1_mccollum/AppLib/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLib
+{
+    public class MathMenu
+    {
+        public static int Add(int input_1, int input_2)
+        {
+            int result = input_1 + input_2;
+            return result;
+        }
+
+            public static int Subtract(int num1, int num2) { return num1 - num2; }
+
+        public static int Divide(int num1, int num2) { return num1 / num2; }
+
+        public static int Multiply(int num1, int num2) { return num1 * num2; }
+    }
+}
